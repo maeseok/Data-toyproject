be4b7a6a59c53ee32180367d7c8fef54dbced4b6
maeseok <yunbum37@daum.net>
2023-05-15 22:48:16 +0900

    0515

--- Data/Dow.py
+++ Data/Dow.py
@@ -11,7 +11,7 @@ close=[]
 diff=[]
 rate=[]
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.62'}
-for i in range(1,100):
+for i in range(1,520):
             url="https://finance.naver.com/world/worldDayListJson.naver?symbol=DJI@DJI&fdtc=0&page="+str(i)
             data = requests.get(url,headers=headers)
             value= data.json()         
@@ -25,3 +25,4 @@ for i in range(1,100):
             df=pd.DataFrame({"code":symb, "date":xymd,"open":open,"close":close,
                                   "diff":diff,"rate":rate})
 print(df)
+print(df.isna().sum())
--- Data/kospi.py
+++ Data/kospi.py
@@ -3,7 +3,8 @@ import pandas as pd
 import urllib.request as req
 import requests
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.62'}
-for i in range(1,2):
+Data=pd.DataFrame()
+for i in range(1,27):
             url="https://m.stock.naver.com/api/json/sise/dailySiseIndexListJson.nhn?code=KOSPI&pageSize=200&page="+str(i)
             data = requests.get(url,headers=headers)
             value= data.json()   
@@ -11,4 +12,8 @@ for i in range(1,2):
             df=pd.DataFrame(kospi)
             df=df[['cd','dt','ov','ncv','cv','cr']] #특정 열 선택
             df = df.rename(columns={'cd':'code',"dt":"date",'ov':'open','ncv':'close','cv':'diff','cr':'rate'}) #열 이름 변경
-print(df)
+            Data=pd.concat([Data,df])
+            #index를 reset해주고, 인덱스 열을 버린다.
+            Data=Data.reset_index(drop=True)
+print(Data)
+print(Data.isna().sum())
