6ffdab44940530501c6e4aaa391cf31cdc94b314
maeseok <yunbum37@daum.net>
2023-06-17 22:23:53 +0900

    0617

--- test.py
+++ test.py
@@ -9,9 +9,10 @@ def fy(x,y):
     return -2*x+8*y
 # initialization of variables 
 x0,y0=1.0, 2.0
-#입실론과 t의 값 변화에 따른 iter의 변화 주의할 것
+#입실론과 t의 값 변화에 따른 iter의 변화 주의할 것 - 입실론 커지고, t 커질수록 iter값이 감소하게 된다.
+#하지만 너무 큰 eps 값은 정확한 iter값을 얻지 못할 수 있다. 
 eps = 1e-6
-t=0.1
+t=0.01
 # make lists 
 xlist, ylist=[],[]
 xlist.append(x0)
@@ -28,6 +29,7 @@ while True:
     
     xlist.append(xkp1)
     ylist.append(ykp1)
+    #eps보다 작으면
     if np.linalg.norm(np.array((xkp1-xk, ykp1-yk)))<eps:
         print("GD converges at iter =", iter)
         break
